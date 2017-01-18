954b8d63a4520f5de53de769ac20dfaa61a32d3a
lifangyuan <lifangyuan@hnbenz.com>
2017-01-18 15:04:43 +0800

    sssd

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('xxxxxxxxxxxxxxxxxxxxxxx');
+  res.send('xxxxxxxxxxxxxxxxxxxxxxxssssss');
 });
 
 module.exports = router;
