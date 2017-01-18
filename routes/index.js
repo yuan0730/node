4894c7f4a1fde52262a870c2da33e19800f3dcf7
lifangyuan_home <lifangyuan@home>
2017-01-18 20:26:18 +0800

    sssss

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FangYuan'});
+  res.render('index', { title: 'liFangYuan'});
 });
 
 module.exports = router;
