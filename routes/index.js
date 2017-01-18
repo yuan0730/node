4000284fcf790cbeda58c842dfe114e924736d14
lifangyuan <lifangyuan@hnbenz.com>
2017-01-18 14:55:29 +0800

    sss

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  console.log('1');
-  next('router');
-},function(req,res,next){
-console.log('22222');
-next();
+  res.render('index', { title: 'Express' });
 });
-router.get('/sss',function(req,res,next){
-        console.log('333333333');
-        });
 
 module.exports = router;
